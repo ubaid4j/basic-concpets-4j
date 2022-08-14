ff0188db4847565b7b376a2f0a21170624f32e03
UbaidurRehman1 <ubaidshan007@gmail.com>
2022-08-14 15:14:30 +0500

    - add item 10 tests

--- effective-java/src/test/java/com/ubaid/forj/Item10Test.java
+++ effective-java/src/test/java/com/ubaid/forj/Item10Test.java
@@ -1,6 +1,8 @@
 package com.ubaid.forj;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
 
 // OBEY THE GENERAL CONTRACT WHEN OVERRIDING EQUALS
@@ -11,10 +13,10 @@ public class Item10Test {
         CaseInsensitiveStringViolationSymmetry cis = new CaseInsensitiveStringViolationSymmetry("Polish");
         String s = "polish";
 
-        Assertions.assertEquals(cis, s, cis + " should equal to " + s);
+        assertEquals(cis, s, cis + " should equal to " + s);
 
         //violating symmetry
-        Assertions.assertNotEquals(s, cis, s + " should equal to " + cis);
+        assertNotEquals(s, cis, s + " should equal to " + cis);
         
     }
 
@@ -23,10 +25,10 @@ public class Item10Test {
         CaseInsensitiveStringFollowingSecondContract cis = new CaseInsensitiveStringFollowingSecondContract("Polish");
         String s = "polish";
 
-        Assertions.assertNotEquals(cis, s, cis + " should equal to " + s);
+        assertNotEquals(cis, s, cis + " should equal to " + s);
 
         //violating symmetry
-        Assertions.assertNotEquals(s, cis, s + " should equal to " + cis);
+        assertNotEquals(s, cis, s + " should equal to " + cis);
     }
     
     @Test
@@ -35,9 +37,43 @@ public class Item10Test {
         ColorPointViolateSymmetry cp1 = new ColorPointViolateSymmetry(1, 2, 1);
         
         // this assertions demonstrate symmetry violation
-        Assertions.assertEquals(p1, cp1);
-        Assertions.assertNotEquals(cp1, p1);
+        assertEquals(p1, cp1);
+        assertNotEquals(cp1, p1);
         
+    }
+    
+    @Test
+    void testColorPointTransitivityViolation() {
+        Point p1 = new ColorPointViolateTrasitivity(1, 2, 1);
+        Point p2 = new Point(1, 2);
+        Point p3 = new ColorPointViolateTrasitivity(1, 2, 2);
+        
+        //symmetry passed but
+        assertEquals(p1, p2, p1 + " should equal to " + p2);
+        assertEquals(p2, p1, p1 + " should equal to " + p2);
+        assertEquals(p2, p3, p2 + " should equal to " + p3);
+        assertEquals(p2, p2, p2 + " should equal to " + p3);
+        
+        //if above are true then following p1 and p3 should be equal
+        //transitivity failed
+        assertNotEquals(p1, p3, p1 +  " should equal to " + p3);
+    }
+    
+    @Test
+    void testSymmetryAndTransitivity() {
+        Point point = new Point(1, 2);
+        ColorPoint p1 = new ColorPoint(point, 1);
+        ColorPoint p2 = new ColorPoint(point, 1);
+        ColorPoint p3 = new ColorPoint(point, 1);
+
+        assertEquals(p1, p2, p1 + " should equal to " + p2);
+        assertEquals(p2, p1, p1 + " should equal to " + p2);
+        assertEquals(p2, p3, p2 + " should equal to " + p3);
+        assertEquals(p2, p2, p2 + " should equal to " + p3);
+        assertEquals(p1, p3, p1 +  " should equal to " + p3);
+
+        assertNotEquals(point, p1);
+        assertNotEquals(p1, point);
     }
 
 
@@ -99,8 +135,8 @@ class Point {
 
     @Override
     public boolean equals(Object o) {
+        if (o == this) return true;
         if (!(o instanceof Point)) return false;
-        
         Point p = (Point) o;
         return p.x == x && p.y == y;
     }
@@ -136,4 +172,59 @@ class ColorPointViolateSymmetry extends Point {
             '}';
     }
 }
+
+class ColorPointViolateTrasitivity extends Point {
+    private final int color;
+
+    public ColorPointViolateTrasitivity(int x, int y, int color) {
+        super(x, y);
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Point)) return false;
+        
+        // on normal point, do a color-blind comparison
+        if (!(o instanceof ColorPointViolateTrasitivity)) return o.equals(this);
+        
+        // on color point, do a full comparison
+        return super.equals(o) && ((ColorPointViolateTrasitivity) o).color == color;
+    }
+
+    @Override
+    public String toString() {
+        return "ColorPointViolateTrasitivity{" +
+            super.toString() +
+            "color=" + color +
+            '}';
+    }
+}
+
+//This class follow symmetry and transitivity 
+class ColorPoint {
+    private final Point point;
+    private final int color;
+
+    public ColorPoint(Point point, int color) {
+        this.point = point;
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ColorPoint)) return false;
+        ColorPoint cp = (ColorPoint) o;
+        return cp.point.equals(point) && cp.color == color;
+    }
+    
+    @Override
+    public String toString() {
+        return "ColorPoint{" +
+            "point=" + point +
+            ", color=" + color +
+            '}';
+    }
+}
+
 
