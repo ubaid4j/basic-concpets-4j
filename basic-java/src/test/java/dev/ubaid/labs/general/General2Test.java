e7a691c0be4ccd05e41e1c646560c523ca4ed8bb
ubaid4j <ubaidshan007@gmail.com>
2024-01-05 09:30:31 +0500

    - general test

--- basic-java/src/test/java/dev/ubaid/labs/general/General2Test.java
+++ basic-java/src/test/java/dev/ubaid/labs/general/General2Test.java
@@ -6,85 +6,93 @@ import org.junit.jupiter.api.Test;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.Comparator.reverseOrder;
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toCollection;
 
 @Slf4j
 public class General2Test {
-    
+
+
+    //custom entry that holds a list of characters and their occurrence
+    record OccurrenceEntry(List<String> characters, Long occurrence) {
+        private static OccurrenceEntry from(Map.Entry<String, Long> entry) {
+            return new OccurrenceEntry(List.of(entry.getKey()), entry.getValue());
+        }
+    }
+
+    // A custom list that will merge the same occurrence elements in same entry
+    // like let suppose if we have 
+    // two entries c -> 2 and d -> 2 that needs to add in this list.  
+    //this list will merge above entries into one  [c, d] -> 2 entry and add this entry into the list 
+    static class OccurrenceList extends AbstractList<OccurrenceEntry> {
+
+        private final Map<Long, List<String>> internalOccuranceMap = new HashMap<>();
+        private final List<OccurrenceEntry> list = new LinkedList<>();
+
+        //If entry occurrence value is same with already existed entry in the list
+        //then merge the characters of this entry with already existed ones and put entry in the list.
+        @Override
+        public boolean add(OccurrenceEntry occurrenceEntry) {
+            if (internalOccuranceMap.containsKey(occurrenceEntry.occurrence())) {
+                List<String> newCharacter = new ArrayList<>(occurrenceEntry.characters());
+                List<String> existingCharacters = new ArrayList<>(internalOccuranceMap.get(occurrenceEntry.occurrence()));
+                List<String> allCharactersWithSameOccurrence = Stream.concat(existingCharacters.stream(), newCharacter.stream()).toList();
+                internalOccuranceMap.put(occurrenceEntry.occurrence(), allCharactersWithSameOccurrence);
+                list.remove(new OccurrenceEntry(existingCharacters, occurrenceEntry.occurrence)); //time complexity O(n)
+                OccurrenceEntry newOccurrenceEntry = new OccurrenceEntry(allCharactersWithSameOccurrence, occurrenceEntry.occurrence);
+                return list.add(newOccurrenceEntry);
+            }
+            internalOccuranceMap.put(occurrenceEntry.occurrence(), occurrenceEntry.characters());
+            return list.add(occurrenceEntry);
+        }
+
+        @Override
+        public OccurrenceEntry get(int index) {
+            return list.get(index);
+        }
+
+        @Override
+        public int size() {
+            return list.size();
+        }
+    }
+
     
     /*Given a string, find the character with 2nd most occurrences.
     Input: "abbbcdabcda"
     Output: "a"*/
-    
+
+    //total time complexity: O(n log n) for worst case scenario
     @Test
     void getSecondMostOccurredCharacterInGivenString() {
         final String input = "abbbcdabcda";
 
-        //custom entry that holds a list of characters and their occurrence
-        record OccurrenceEntry(List<String> characters, Long occurrence) {
-            private static OccurrenceEntry from(Map.Entry<String, Long> entry) {
-                return new OccurrenceEntry(List.of(entry.getKey()), entry.getValue());
-            }
-        }
-        
-        // A custom list that will merge the same occurrence elements in same entry
-        // like let suppose if we have 
-        // two entries c -> 2 and d -> 2 that needs to add in this list.  
-        //this list will merge above entries into one  [c, d] -> 2 entry and add this entry into the list 
-        class OccurrenceList extends AbstractList<OccurrenceEntry> {
-
-            private final Map<Long, List<String>> internalOccuranceMap = new HashMap<>();
-            private final List<OccurrenceEntry> list = new ArrayList<>(); 
-
-            @Override
-            public boolean add(OccurrenceEntry occurrenceEntry) {
-                if (internalOccuranceMap.containsKey(occurrenceEntry.occurrence())) {
-                    List<String> newCharacter = new ArrayList<>(occurrenceEntry.characters());
-                    List<String> existingCharacters = new ArrayList<>(internalOccuranceMap.get(occurrenceEntry.occurrence()));
-                    List<String> allCharactersWithSameOccurrence = Stream.concat(existingCharacters.stream(), newCharacter.stream()).toList();
-                    internalOccuranceMap.put(occurrenceEntry.occurrence(), allCharactersWithSameOccurrence);
-                    list.remove(new OccurrenceEntry(existingCharacters, occurrenceEntry.occurrence));
-                    OccurrenceEntry newOccurrenceEntry = new OccurrenceEntry(allCharactersWithSameOccurrence, occurrenceEntry.occurrence);
-                    return list.add(newOccurrenceEntry);
-                }
-                internalOccuranceMap.put(occurrenceEntry.occurrence(), occurrenceEntry.characters());
-                return list.add(occurrenceEntry);
-            }
-
-            @Override
-            public OccurrenceEntry get(int index) {
-                return list.get(index);
-            }
-
-            @Override
-            public int size() {
-                return list.size();
-            }
-        }
-
         List<OccurrenceEntry> occurenceList = input.chars()
                 .mapToObj(Character::toString) //get list of character
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())) //get a map character -> occurrence of char
+                .collect(groupingBy(identity(), counting())) //get a map character -> occurrence of char
                 .entrySet()
                 .stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) //sort that map in desc order by occurrence of characters (value)
+                .sorted(comparingByValue(reverseOrder())) //sort that map in desc order by occurrence of characters (value) time complexity for worst case: O(n log n)
                 .map(OccurrenceEntry::from) //map to custom object
-                .collect(Collectors.toCollection(OccurrenceList::new)); // collect in custom list that will have logic to merge same occurrence elements 
+                .collect(toCollection(OccurrenceList::new)); // collect in custom list that will have logic to merge same occurrence elements 
 
         log.info("{}", occurenceList);
 
         Assertions.assertEquals(List.of("a"), occurenceList.get(1).characters(), "second most occurred element should be a");
 
-        
+
         //other tests to verify all scenarios
         Assertions.assertEquals(List.of("b"), occurenceList.getFirst().characters(), "first most occurred element should be b");
         Assertions.assertEquals(List.of("c", "d"), occurenceList.get(2).characters(), "third most occurred element should be c and d");
-
     }
 }
--- spring-bean-factory/src/test/java/com/ubaid/forj/annotation/Engine.java
+++ spring-bean-factory/src/test/java/com/ubaid/forj/annotation/Engine.java
@@ -2,14 +2,26 @@ package com.ubaid.forj.annotation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Engine {
+public class Engine implements InitializingBean, DisposableBean {
     
     static Logger logger = LoggerFactory.getLogger(Engine.class);
     
     public void start() {
         logger.debug("starting engine");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
     }
 }
