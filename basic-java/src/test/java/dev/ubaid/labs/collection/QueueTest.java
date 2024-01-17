994bc620eda473e11918d3fce41fd1a2984bf47a
ubaid4j <ubaidshan007@gmail.com>
2024-01-17 08:42:52 +0500

    - collection test

--- basic-java/src/test/java/dev/ubaid/labs/collection/QueueTest.java
+++ basic-java/src/test/java/dev/ubaid/labs/collection/QueueTest.java
@@ -1,6 +1,7 @@
 package dev.ubaid.labs.collection;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.NoSuchElementException;
 import java.util.Queue;
@@ -33,8 +34,78 @@ public class QueueTest {
         Assertions.assertEquals("Head", q1.poll());
     }
     
-    //TODO Tue Jan 16 9:41:30 AM
-    //queue with array dequeue, linkedList
-    //stack with array dequeue, linkedList
-    //priority queue
+    @Test
+    void stackTest() {
+        Deque<Integer> stack = new LinkedList<>();
+        
+        stack.add(1);
+        stack.add(2);
+        stack.add(3);
+        
+        Integer three = stack.pollLast();
+        Assertions.assertEquals(3, three);
+        
+        stack.add(2);
+        
+        Integer two = stack.pollLast();
+        Assertions.assertEquals(2, two);
+        
+        two = stack.pollLast();
+        Assertions.assertEquals(2, two);
+        
+        Integer one = stack.pollLast();
+        Assertions.assertEquals(1, one);
+        
+        stack.add(6);
+        stack.add(10);
+        
+        Integer ten = stack.pollLast();
+        Assertions.assertEquals(10, ten);
+    }
+    
+    @Test
+    void queueTest() {
+        Deque<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        
+        Integer one = queue.poll();
+        Assertions.assertEquals(1, one);
+        
+        Integer two = queue.poll();
+        Assertions.assertEquals(2, two);
+        
+        queue.add(4);
+        queue.add(5);
+        
+        Integer three = queue.poll();
+        Assertions.assertEquals(3, three);
+        
+        Integer four = queue.poll();
+        Assertions.assertEquals(4, four);
+    }
+    
+    @Test
+    void queueTest2() {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        
+        Integer one = queue.poll();
+        Assertions.assertEquals(1, one);
+        
+        Integer two = queue.poll();
+        Assertions.assertEquals(2, two);
+        
+        queue.add(4);
+        queue.add(5);
+        
+        Integer three = queue.poll();
+        Assertions.assertEquals(3, three);
+        
+        Integer four = queue.poll();
+        Assertions.assertEquals(4, four);
+    }
 }
