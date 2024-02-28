f5160720e3b79dfe360202ffe6204ef40f285687
ubaid4j <ubaidshan007@gmail.com>
2024-02-28 09:19:40 +0500

    - enhancement

---
+++ basic-java/src/test/java/dev/ubaid/labs/general/General6Test.java
@@ -0,0 +1,13 @@
+package dev.ubaid.labs.general;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class General6Test {
+    
+    @Test
+    void test1() {
+        log.info("Hello World");
+    }
+}
