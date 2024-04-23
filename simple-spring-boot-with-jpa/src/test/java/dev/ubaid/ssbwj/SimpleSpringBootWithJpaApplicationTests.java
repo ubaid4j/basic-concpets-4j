6b6e25c8bf341cd3b67334f203b556f83e73a24e
ubaid4j <ubaidshan007@gmail.com>
2024-04-23 13:11:01 +0500

    - github action for simple spring boot with jpa

--- simple-spring-boot-with-jpa/src/test/java/dev/ubaid/ssbwj/SimpleSpringBootWithJpaApplicationTests.java
+++ simple-spring-boot-with-jpa/src/test/java/dev/ubaid/ssbwj/SimpleSpringBootWithJpaApplicationTests.java
@@ -1,13 +1,24 @@
 package dev.ubaid.ssbwj;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.test.context.TestPropertySource;
+
 
 @SpringBootTest
+@TestPropertySource(
+        properties = {
+                "spring.datasource.url=jdbc:tc:postgresql:16.2:///testdb"
+        }
+)
 class SimpleSpringBootWithJpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ConfigurableEnvironment env;
 
+    @Test
+    void contextLoads() {
+    }
 }
